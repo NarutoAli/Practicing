0259545096daac0bc80c96a90c16ab251a8cd590
NarutoAli <40733375@live.napier.ac.uk>
2025-04-12 17:14:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let fullName = 'rayan ali';
+let age = 22;
+let isStudent = true;
+
+document.getElementById('p1')
+.textContent = `My full name is ${fullName}`;
+
+document.getElementById('p2')
+.textContent = `My age is ${age}`;
+
+document.getElementById('p3')
+.textContent = `Enrolled in School: ${isStudent}`;
+
+
+let students = 30;
+
+students *= 2;
+
+console.log(students);
+
+
